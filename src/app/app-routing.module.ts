696d129d8ae2e27bc7a75feffcbc1c15ad4e5611
Ewerton <ewerton.lfb@gmail.com>
2024-10-04 02:00:17 -0300

    resolve router bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'country-detail/:numericCode',
+    loadChildren: () =>
+      import('./pages/country-detail/country-detail.module').then(
+        (m) => m.CountryDetailModule
+      ),
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Redireciona para a Home em caso de rota não encontrada
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
